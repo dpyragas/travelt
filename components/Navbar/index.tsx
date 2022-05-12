ed20c4987101d82e5866d7459389fa41ca4720d8
Daumantas PYRAGAS <dpyragas@SideTrade.com>
2022-05-12 17:29:26 +0100

    Navbar styles saved

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,4 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const NavbarTw = styled.div`
+  ${tw`
+        flex
+        h-20
+        px-6
+        pt-4
+        border-b-2
+        border-gray-200
+        border-opacity-50
+        items-center
+        self-center
+        `}
+`;
+const NavigationItems = tw.ul`
+    list-none
+    w-full
+    h-auto
+    
+`;
 
 const Navbar = () => {
   return <div>Navbar</div>;
